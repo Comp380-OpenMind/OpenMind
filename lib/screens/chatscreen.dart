f7b0adf5206895db6b60a22ce04a20c97882463a
Daniel Monascal <danielmonascal@gmail.com>
2021-03-27 18:10:08 -0700

    base chatscreen and some changes to version control

--- lib/screens/chatscreen.dart
+++ lib/screens/chatscreen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:open_mind/helperfunctions/sharedpref_helper.dart';
 
 class ChatScreen extends StatefulWidget {
   final String chatWithUsername, name;
@@ -8,8 +9,74 @@ class ChatScreen extends StatefulWidget {
 }
 
 class _ChatScreenState extends State<ChatScreen> {
+  String chatRoomId;
+  String myName, myProfilePic, myUserName, myEmail;
+
+  getMyInfoFromSharedPreference() async {
+    myName = await SharedPreferenceHelper().getDisplayName();
+    myProfilePic = await SharedPreferenceHelper().getUserProfileUrl();
+    myUserName = await SharedPreferenceHelper().getUserName();
+    myEmail = await SharedPreferenceHelper().getUserEmail();
+
+    chatRoomId = getChatRoomIdByUsernames(widget.chatWithUsername, myUserName);
+  }
+
+  getChatRoomIdByUsernames(String a, String b) {
+    if (a.substring(0, 1).codeUnitAt(0) > b.substring(0, 1).codeUnitAt(0)) {
+      return "$b\_$a";
+    } else {
+      return "$a\_$b";
+    }
+  }
+
+  getAndSetMessages() async {}
+
+  doThisOnLaunch() async {
+    await getMyInfoFromSharedPreference();
+    getAndSetMessages();
+  }
+
+  @override
+  void initState() {
+    doThisOnLaunch();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    // Creates a bar at the top with the name
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.name),
+      ),
+      body: Container(
+        child: Stack(
+          children: [
+            Container(
+              alignment: Alignment.bottomCenter,
+              child: Container(
+                  // makes the widget an opaque black
+                  color: Colors.black.withOpacity(0.8),
+                  padding: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+                  child: Row(
+                    children: [
+                      Expanded(
+                          // text the user inputs
+                          child: TextField(
+                        style: TextStyle(color: Colors.white),
+                        decoration: InputDecoration(
+                            border: InputBorder.none,
+                            hintText: "Type a message",
+                            hintStyle: TextStyle(
+                                color: Colors.white.withOpacity(0.6))),
+                      )),
+                      Icon(Icons.send, color: Colors.white)
+                    ],
+                  )),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
